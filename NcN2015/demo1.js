d4807512df1ce129600f089d1c29a6f768f8a0d3
Ole André Vadla Ravnås <oleavr@nowsecure.com>
2015-12-13 09:01:00 +0100

    Add source code for NcN demos
    Demoed by using Frida's REPL to inject them into a running
    process: `frida <target-process> -l demo1.js`

---
+++ NcN2015/demo1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Interceptor.attach(Module.findExportByName('libSystem.B.dylib', 'open'), {
+  onEnter(args) {
+    const path = Memory.readUtf8String(args[0]);
+    console.log('open("' + path + '")');
+  }
+});
---
+++ NcN2015/demo2.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const NSSpeechSynthesizer = ObjC.classes.NSSpeechSynthesizer;
+
+const synth = NSSpeechSynthesizer.alloc().init();
+const voiceId = NSSpeechSynthesizer.availableVoices().objectAtIndex_(0);
+synth.setVoice_(voiceId);
+
+Interceptor.attach(Module.findExportByName('libSystem.B.dylib', 'open'), {
+  onEnter(args) {
+    const path = Memory.readUtf8String(args[0]);
+    const name = path.substr(path.lastIndexOf('/') + 1);
+    console.log('OPENING "' + name + '"');
+    synth.startSpeakingString_(name);
+  }
+});
+
